04c20aa51b8270dbfdcb1f4072815ea6296f1c92
Santos Osmin Urrutia Iraheta <urrutia.osmin@gmail.com>
2018-08-13 14:29:24 -0600

    Correccion del tamaño del String en el provider y anade Billable  User

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        Schema::defaultStringLength(195);
+        Schema::defaultStringLength(191);
     }
 
     /**
