877d9421cdc44c8fd6a502e355b4d43a9ef9afda
WalexTobar <tobarvalladaresalexis@gmail.com>
2018-08-20 21:18:54 -0600

    añadi el logo, cree cajas para precios solo son de prueba,creare unas con mis propios estilos (pendiente)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,23 +17,49 @@
 </head>
 <body>
   <nav class="cyan darken-4" role="navigation">
-    <div class="nav-wrap    per container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="#">Navbar Link</a></li>
-        <li><a href="">navbar link</a></li>
-      </ul>
-
-      <ul id="nav-mobile" class="side-nav">
-        <li><a href="#">Navbar Link</a></li>
-        <li><a href="#">Navbar Link</a></li>
-      </ul>
-      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    <div class="nav-wrapper container">
+        <a id="logo-container" href="#" class="brand-logo">
+        <!--añado una imagen svg-->
+          <img style="max-height: 100px;" src="{{ asset('img/Logo.svg') }}" alt="Logo">
+        </a>
+      <!--icono tres lineas-->
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="#">Navbar Link</a></li>
+            <li><a href="">navbar link</a></li>
+            <li><a  data-target="slide-out" class="waves-effect waves-light outnav-trigger btn">User<i class="material-icons right">account_circle</i></a></li></li>
+          </ul>
     </div>
   </nav>
+       <ul class="sidenav" id="mobile-demo">
+        <li><a href="#">Navbar Link</a></li>
+        <li><a href="#">Navbar Link</a></li>
+        
+      </ul>
+      <!--info-user - faltan hacer pruebas -->
+        <ul id="slide-out" class="sidenav">
+    <li><div class="user-view">
+      <div class="background">
+        <img src="images/office.jpg">
+      </div>
+      <a href="#user"><img class="circle" src="images/yuna.jpg"></a>
+      <a href="#name"><span class="white-text name">John Doe</span></a>
+      <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
+    </div></li>
+    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+    <li><a href="#!">Second Link</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">Subheader</a></li>
+    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+  </ul>
+
+      
+  
+  
   <div class="section no-pad-bot" id="index-banner">
     <div class="container" id="banner">
       <br><br>
-      <h1 style="text-anchor: bold;" class="header center cyan-text text-darken-3">Bienvenido</h1>
+      <h1 class="header center cyan-text text-darken-3">Bienvenido</h1>
       <div class="row center">
         <h5 class="header col s12 light">Hello World11</h5>
       </div>
@@ -83,8 +109,40 @@
     <br><br>
 
     <div class="section">
-
+        <div class="row">
+            <div class="col s12"> 
+                <h1 class="orange-text lighten-5 center">Precios</h1>
+            </div>
+            <div class="col s12 m4">
+                <div class="card cyan lighten-5">
+                    <div class="card-content black-text">
+                        <span class="card-title center">Mensual</span>
+                        <p> $15</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card cyan lighten-5">
+                    <div class="card-content black-text">
+                        <span class="card-title center">Trimestral</span>
+                        <p> $30</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card cyan lighten-5">
+                   <div class="card-content black-text">
+                       <span class="card-title center">año</span>
+                       <p>  $90</p>
+                           
+                      
+                   </div> 
+                </div>
+            </div>
+        </div>
     </div>
+    <br>
+    <br>
   </div>
 <!--init footer-->
   <footer class="page-footer cyan darken-4">
@@ -128,7 +186,7 @@
   <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="{{ asset('js/materialize.min.js') }}"></script>
-  <script src="js/init.js"></script>
+  <script src="{{ asset('js/myScript.js') }}"></script>
 
   </body>
 </html>
