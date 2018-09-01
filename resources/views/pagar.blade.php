c3d376651df7e691b99336df055daa2e0030b933
Santos Osmin Urrutia Iraheta <urrutia.osmin@gmail.com>
2018-09-01 15:11:31 -0600

    arreglando datos en el customStripe.js

--- resources/views/pagar.blade.php
+++ resources/views/pagar.blade.php
@@ -11,24 +11,25 @@
 					<div class="col s12 m4"></div>
 					<div class="col s12 m4">
 						<div class="row">
-							<form class="col s12" action="/charge" method="post" id="payment-form">
+							<form class="col s12" action="{{ route('cashier') }}" method="post" id="payment-form">
+								{{ csrf_field() }}
 								<div class="form-row">
 									<label for="card-numero">
 									 	N&uacute;mero de Tajeta
 									</label>
 								<div id="card-numero">
-
 								</div>
+								<!-- Used to display Element errors. -->
+								<div id="card-errors" role="alert"></div>
 								<br>
 								<div id="card-exp"></div>
 								<div id="card-cvc" ></div>
 								<input type="text" placeholder="Nombre en la tarjeta">
-								<!-- Used to display Element errors. -->
-								<div id="card-errors" role="alert"></div>
+								
 							  </div>
 							
 							  <button>Pagar</button>
-							 </form>
+							</form>
 						</div>
 					</div><!--Elemento Centrado-->
 					<div class="col s12 m4"></div>
