07566b3b5c11ad643b0e95ce567d0de17e332a41
Santos Osmin Urrutia Iraheta <urrutia.osmin@gmail.com>
2018-09-01 14:34:53 -0600

    solucion a error de libreria de stripe

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -121,9 +121,8 @@
 	<!--  Scripts-->
 	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 	<script src="{{ asset('js/materialize.min.js') }}"></script>
-	<script src="{{ asset('js/myScript.js') }}"></script>
-	<script src="https://js.stripe.com/v3/"></script><!--add libreria Stripe-->
 	@yield('scripts')
+	<script src="{{ asset('js/myScript.js') }}"></script>
 	</body>
 </html>
 										
--- resources/views/pagar.blade.php
+++ resources/views/pagar.blade.php
@@ -4,37 +4,42 @@
 
 @section('content')
 <div class="container">
-    <section>
-        <div class="section">
-            <article>
-                <div class="row">
-                    <div class="col s12 m4"></div>
-                    <div class="col s12 m4">
-                        <div class="row">
-                            <form class="col s12" action="/charge" method="post" id="payment-form">
-                                <div class="form-row">
-                                <label for="card-numero">
-                                 N&uacute;mero de Tajeta
-                                </label>
-                                <div id="card-numero">
-                                </div>
-                                <br>
-                                <div id="card-exp"></div>
-                                <div id="card-cvc" ></div>
-                                <input type="text" placeholder="Nombre en la tarjeta">
-                                <!-- Used to display Element errors. -->
-                                <div id="card-errors" role="alert"></div>
-                              </div>
-                            
-                              <button>Pagar</button>
-                             </form>
-                        </div>
-                    </div><!--Elemento Centrado-->
-                    <div class="col s12 m4"></div>
-                </div>
-            </article>
-        </div>
-    </section>
+	<section>
+		<div class="section">
+			<article>
+				<div class="row">
+					<div class="col s12 m4"></div>
+					<div class="col s12 m4">
+						<div class="row">
+							<form class="col s12" action="/charge" method="post" id="payment-form">
+								<div class="form-row">
+									<label for="card-numero">
+									 	N&uacute;mero de Tajeta
+									</label>
+								<div id="card-numero">
+
+								</div>
+								<br>
+								<div id="card-exp"></div>
+								<div id="card-cvc" ></div>
+								<input type="text" placeholder="Nombre en la tarjeta">
+								<!-- Used to display Element errors. -->
+								<div id="card-errors" role="alert"></div>
+							  </div>
+							
+							  <button>Pagar</button>
+							 </form>
+						</div>
+					</div><!--Elemento Centrado-->
+					<div class="col s12 m4"></div>
+				</div>
+			</article>
+		</div>
+	</section>
 </div>
 
+@endsection
+
+@section('scripts')
+	<script src="https://js.stripe.com/v3/"></script><!--add libreria Stripe-->
 @endsection
